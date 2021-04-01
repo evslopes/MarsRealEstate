0eaa5b8b29cf923a80658e0402005165e97179f6
Elvis Lopes <elvis.lopes@al.infnet.edu.br>
2021-04-01 08:49:55 -0300

    Criando uma página de detalhes mais útil

--- app/src/main/java/com/example/android/marsrealestate/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/detail/DetailViewModel.kt
@@ -17,10 +17,8 @@
 package com.example.android.marsrealestate.detail
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.example.android.marsrealestate.R
 import com.example.android.marsrealestate.network.MarsProperty
 
 /**
@@ -39,5 +37,25 @@ class DetailViewModel(
         _selectedProperty.value = marsProperty
     }
 
+    //Esta transformação testa se a propriedade selecionada é uma locação,
+    // Se a propriedade for alugada, a transformação escolhe a string apropriada
+    // dos recursos com uma when {}chave Kotlin
+    val displayPropertyPrice = Transformations.map(selectedProperty) {
+        app.applicationContext.getString(
+                when (it.isRental) {
+                    true -> R.string.display_price_monthly_rental
+                    false -> R.string.display_price
+                }, it.price)
+    }
+
+    //Essa transformação concatena vários recursos de string, com base no fato de o tipo de propriedade ser um aluguel
+    val displayPropertyType = Transformations.map(selectedProperty) {
+        app.applicationContext.getString(R.string.display_type,
+                app.applicationContext.getString(
+                        when (it.isRental) {
+                            true -> R.string.type_rent
+                            false -> R.string.type_sale
+                        }))
+    }
 
 }
