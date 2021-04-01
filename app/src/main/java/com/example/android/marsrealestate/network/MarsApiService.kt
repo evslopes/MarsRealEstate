7d39d1ac663602af99e9cc00e72e3adcd283a602
Elvis Lopes <elvis.lopes@al.infnet.edu.br>
2021-04-01 08:10:23 -0300

    Filtrando os resultados - atualizando o serviço API Mars

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -23,6 +23,12 @@ import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Query
+
+enum class MarsApiFilter(val value: String) {
+    SHOW_RENT("rent"),
+    SHOW_BUY("buy"),
+    SHOW_ALL("all") }
 
 private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com/"
 
@@ -36,10 +42,8 @@ private val retrofit = Retrofit.Builder()
         .build()
 
 interface MarsApiService{
-    @GET("realestate")
-    suspend fun getProperties():
-            List<MarsProperty>
-}
+    //@GET("realestate")
+    suspend fun getProperties(@Query("filter") type: String): List<MarsProperty>  }
 
 object MarsApi{
     val retrofitService : MarsApiService by lazy {
